88cea33953483bb33e948d52da894b9fc9c3e72e
bundle.js <bundle.js@gmail.com>
2018-02-03 01:04:04 +0800

    view image

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -22,7 +22,7 @@
         </div>
       </div>
       <div class="personal-info-image">
-        <img class="personal-info-image-block" src="http://172.16.72.12:8000/3.png" alt="" @click="goTo($event)">
+        <img class="personal-info-image-block" src="https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1517571092&di=85e62d80627d31b4ba2d299d7cdef4a4&src=http://img3.duitang.com/uploads/people/201609/11/20160911214236_xaizW.jpeg" alt="" @click="goTo($event)">
       </div>
     </div>
     <div style="width: 100%;display: flex;justify-content: center">
@@ -30,12 +30,12 @@
         <canvas width="250px" height="250px" ref="screen" v-on="{ mousewheel: mousewheel, click: click, dblclick: dblclick, mousedown: mousedown, mouseup: mouseup, mouseover: mouseover, mouseout: mouseout, mousemove: mousemove, mouseenter: mouseenter, mouseleave: mouseleave }"></canvas>
       </div>
     </div>
-    <input v-model="point1" type="text">
-    <input v-model="point2" type="text">
+    <!--<input v-model="point1" type="text">-->
+    <!--<input v-model="point2" type="text">-->
     <div style="display:none;">
-      <img id="source" src="https://mdn.mozillademos.org/files/5397/rhino.jpg" width="300" height="227" ref="img">
+      <img id="source" src="http://img1.imgtn.bdimg.com/it/u=3300592090,3700440559&fm=27&gp=0.jpg" width="300" height="227" ref="img">
     </div>
-    https://developer.mozilla.org/zh-CN/docs/Web/API/CanvasRenderingContext2D/drawImage
+    <!--https://developer.mozilla.org/zh-CN/docs/Web/API/CanvasRenderingContext2D/drawImage-->
   </div>
 </template>
 
@@ -46,23 +46,42 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App',
       screen: null,
-      point1: 0,
-      point2: 0
+      height: 200,
+      width: 200,
+      x: 0,
+      y: 0,
+      dragging: false
     }
   },
   mounted: function () {
     console.log(this.$refs.screen)
-    this.screen = this.$refs.screen.getContext("2d")
+    this.screen = this.$refs.screen.getContext('2d')
     console.log(this.screen)
-    this.screen.drawImage(this.$refs.img, 0,0)
+    this.screen.drawImage(this.$refs.img, this.x, this.y, this.width, this.height)
+    this.screen.beginPath()
+    this.screen.arc(this.x + this.width * 0.5, this.y + this.height * 0.5, 10, 0, 2 * Math.PI, false)
+    this.screen.stroke()
+    this.screen.beginPath()
+    this.screen.arc(this.x + this.width * 0.3, this.y + this.height * 0.8, 10, 0, 2 * Math.PI, false)
+    this.screen.stroke()
   },
   methods: {
     goTo: function (e) {
       console.log(e)
     },
     mousewheel: function (e) {
-      console.log(e)
-      console.log('canvas坐标:', e.offsetX, ',', e.offsetX)
+      this.width += this.width * e.wheelDelta / 1000
+      this.height += this.height * e.wheelDelta / 1000
+      this.x = e.offsetX - (e.offsetX - this.x) * (1 + e.wheelDelta / 1000)
+      this.y = e.offsetY - (e.offsetY - this.y) * (1 + e.wheelDelta / 1000)
+      this.screen.clearRect(0, 0, 250, 250)
+      this.screen.drawImage(this.$refs.img, this.x, this.y, this.width, this.height)
+      this.screen.beginPath()
+      this.screen.arc(this.x + this.width * 0.5, this.y + this.height * 0.5, 10, 0, 2 * Math.PI, false)
+      this.screen.stroke()
+      this.screen.beginPath()
+      this.screen.arc(this.x + this.width * 0.3, this.y + this.height * 0.8, 10, 0, 2 * Math.PI, false)
+      this.screen.stroke()
     },
     click: function (e) {
       // console.log(e)
@@ -73,10 +92,12 @@ export default {
       // console.log('canvas坐标:', e.offsetX, ',', e.offsetY)
     },
     mousedown: function (e) {
+      this.dragging = true
       // console.log(e)
       // console.log('canvas坐标:', e.offsetX, ',', e.offsetY)
     },
     mouseup: function (e) {
+      this.dragging = false
       // console.log(e)
       // console.log('canvas坐标:', e.offsetX, ',', e.offsetY)
     },
@@ -85,25 +106,31 @@ export default {
       // console.log('canvas坐标:', e.offsetX, ',', e.offsetY)
     },
     mouseout: function (e) {
+      this.dragging = false
       // console.log(e)
       // console.log('canvas坐标:', e.offsetX, ',', e.offsetY)
       this.screen.stroke()
     },
     mousemove: function (e) {
-      // console.log(e)
+      if (this.dragging) {
+        this.x += e.movementX
+        this.y += e.movementY
+        this.screen.clearRect(0, 0, 250, 250)
+        this.screen.drawImage(this.$refs.img, this.x, this.y, this.width, this.height)
+        this.screen.beginPath()
+        this.screen.arc(this.x + this.width * 0.5, this.y + this.height * 0.5, 10, 0, 2 * Math.PI, false)
+        this.screen.stroke()
+        this.screen.beginPath()
+        this.screen.arc(this.x + this.width * 0.3, this.y + this.height * 0.8, 10, 0, 2 * Math.PI, false)
+        this.screen.stroke()
+      }
       // console.log('canvas坐标:', e.offsetX, ',', e.offsetY)
-      this.point1 = e.offsetX;
-      this.point2 = e.offsetY;
-//      this.screen.lineTo(e.offsetX, e.offsetY);
-//      this.screen.stroke()
-//      this.screen.moveTo(e.offsetX,e.offsetY);
+      // this.point1 = e.offsetX
+      // this.point2 = e.offsetY
     },
     mouseenter: function (e) {
-      this.screen.beginPath();
-      this.screen.lineWidth = '1'
-//      this.screen.moveTo(e.offsetX*1.2,e.offsetY*0.6);
-      this.screen.moveTo(e.offsetX,e.offsetY);
-//       console.log(this)
+      // this.screen.beginPath()
+      // this.screen.lineWidth = '1'
       // console.log('canvas坐标:', e.offsetX, ',', e.offsetX)
     },
     mouseleave: function (e) {
